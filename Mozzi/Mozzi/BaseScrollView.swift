b453bc8ef437af979d417267e8e44a0469c24f5a
Zoe <Zoe.urssu@gmail.com>
2023-03-09 16:48:16 +0900

    [mainView] 변경 디자인 반영

---
+++ Mozzi/Mozzi/BaseScrollView.swift
@@ -0,0 +1,9 @@
+//
+//  BaseScrollView.swift
+//  Mozzi
+//
+//  Created by 지희의 MAC on 2023/02/27.
+//
+
+import Foundation
+import SnapKit
---
+++ Mozzi/Mozzi/MainViewController.swift
@@ -0,0 +1,124 @@
+//
+//  MainViewController.swift
+//  Mozzi
+//
+//  Created by 지희의 MAC on 2023/02/25.
+//
+
+import UIKit
+import SnapKit
+import FSCalendar
+
+
+
+class MainViewController: UITabBarController,FSCalendarDelegate,FSCalendarDataSource {
+    
+    lazy var horizontalScrollView: HorizontalScrollView = {
+        let view = HorizontalScrollView()
+        return view
+    }()
+    fileprivate weak var fsCalendar: FSCalendar!
+    lazy var profileButton: UIButton = UIButton()
+    lazy var hambugerButton: UIButton = UIButton()
+    lazy var recentLabel: UILabel = UILabel()
+        
+    override func loadView() {
+        let view = UIView(frame: UIScreen.main.bounds)
+            self.view = view
+    }
+
+      override func viewDidLoad() {
+          super.viewDidLoad()
+          view.backgroundColor = .white
+          configure()
+          insertDataSource()
+         
+      }
+
+      private func configure() {
+          let fsCalendar = FSCalendar(frame: CGRect(x: 30, y: 150, width: 340, height: 400))
+
+          let image = UIImage(named: "notification")
+ 
+          
+          
+          fsCalendar.dataSource = self
+          fsCalendar.delegate = self
+          fsCalendar.layer.borderWidth = 3
+          fsCalendar.layer.borderColor = UIColor(named: "main Color")?.cgColor
+          fsCalendar.layer.shadowOffset = CGSize(width: 0, height: 10)
+          fsCalendar.layer.shadowOpacity = 0.3
+          fsCalendar.layer.shadowRadius = 0
+          fsCalendar.layer.shadowColor = UIColor(named: "Dark Color")?.cgColor
+          
+          self.tabBar.backgroundColor = UIColor(named: "main Color")
+          self.tabBar.tintColor = .red
+          self.tabBar.unselectedItemTintColor = .blue
+          
+          let firstVC = UINavigationController()
+          firstVC.tabBarItem.selectedImage = UIImage(systemName: "message")
+          firstVC.tabBarItem.title = "Recent"
+          firstVC.tabBarItem.image = UIImage(systemName: "message.fill")
+                  
+                  let dummyView = UIViewController()
+                  dummyView.view.backgroundColor = .yellow
+                  dummyView.tabBarItem.title = "Yellow Dummy"
+                  dummyView.tabBarItem.image = UIImage(systemName: "trash.fill")
+                  
+                  viewControllers = [firstVC, dummyView]
+
+          view.addSubview(hambugerButton)
+          view.addSubview(recentLabel)
+          view.addSubview(fsCalendar)
+          view.addSubview(horizontalScrollView)
+          
+          self.fsCalendar = fsCalendar
+          fsCalendar.backgroundColor = .white
+          fsCalendar.layer.cornerRadius = 30
+          
+          fsCalendar.snp.makeConstraints{ make in
+              make.centerX.equalToSuperview()
+              make.top.equalToSuperview().offset(150)
+              make.width.equalTo(340)
+              make.height.equalTo(400)
+          }
+          
+          
+          hambugerButton.setImage(image, for: .normal)
+          hambugerButton.snp.makeConstraints{ make in
+              make.top.equalToSuperview().offset(75)
+              make.right.equalToSuperview().offset(-35)
+              make.width.height.equalTo(40)
+              
+          }
+          
+          recentLabel.text = "최근 구매한 내용"
+          recentLabel.snp.makeConstraints{ make in
+              make.top.equalTo(fsCalendar.snp.bottom).offset(15)
+              make.left.equalTo(30)
+          }
+          
+          horizontalScrollView.snp.makeConstraints { make in
+              make.width.equalToSuperview()
+              make.height.equalTo(100)
+              make.top.equalTo(fsCalendar.snp.bottom).offset(40)
+          }
+
+      }
+
+      private func insertDataSource() {
+          horizontalScrollView.dataSource = Mocks.getDataSource()
+      }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
