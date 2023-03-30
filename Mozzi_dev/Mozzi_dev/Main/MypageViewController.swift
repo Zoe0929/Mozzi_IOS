c2cf965733be056449e70914065f71e0bf2fc8b6
Zoe <Zoe.urssu@gmail.com>
2023-03-31 00:43:40 +0900

    [Mypage]

--- Mozzi_dev/Mozzi_dev/Main/MypageViewController.swift
+++ Mozzi_dev/Mozzi_dev/Main/MypageViewController.swift
@@ -6,20 +6,44 @@
 //
 
 import UIKit
+import SnapKit
 
 class MypageViewController: UITabBarController {
+    
+    let mypageBackgroundImage = UIImage(named: "profileBackground")
+    let addProfileImage = UIImage(named: "addProfile")
+    lazy var mypageBackgroundImageView = UIImageView(image: mypageBackgroundImage)
+    lazy var profileAddButton: UIButton = {
+        let button = UIButton()
+        button.setImage(addProfileImage, for: .normal)
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setView()
         // Do any additional setup after loading the view.
     }
-    
-    private func configure(){
-        view.backgroundColor = .white
-        
+    private func setView(){
+        setViewHierarchy()
+        setLayout()
     }
     
+    private func setViewHierarchy(){
+        view.backgroundColor = .white
+        view.addSubview(mypageBackgroundImageView)
+        view.addSubview(profileAddButton)
+    }
+    
+    private func setLayout(){
+        mypageBackgroundImageView.snp.makeConstraints{make in
+            make.top.leading.width.equalToSuperview()
+        }
+        profileAddButton.snp.makeConstraints{make in
+            make.top.equalToSuperview().offset(60)
+            make.centerX.equalToSuperview()
+        }
+    }
 
     /*
     // MARK: - Navigation
