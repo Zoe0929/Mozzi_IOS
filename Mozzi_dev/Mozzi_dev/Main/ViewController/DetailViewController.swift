e025b93ef43d4de78718bd22165a33a999125bf4
Zoe <Zoe.urssu@gmail.com>
2023-05-16 23:34:50 +0900

    [ADD] 디테일 뷰 요소 추가

---
+++ Mozzi_dev/Mozzi_dev/Main/ViewController/DetailViewController.swift
@@ -0,0 +1,15 @@
+//
+//  DetailViewController.swift
+//  Mozzi_dev
+//
+//  Created by 지희의 MAC on 2023/05/16.
+//
+
+import UIKit
+import SnapKit
+
+class DetailViewController: UIViewController {
+    override func viewDidLoad() {
+        
+    }
+}
--- Mozzi_dev/Mozzi_dev/SceneDelegate.swift
+++ Mozzi_dev/Mozzi_dev/SceneDelegate.swift
@@ -20,7 +20,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         let mainTabBarController = MainTabBarController()
         // 맨 처음 보여줄 ViewController
 
-        window?.rootViewController = mainTabBarController
+        window?.rootViewController = mainViewController
         window?.makeKeyAndVisible()
         window?.windowScene = windowScene
     }
